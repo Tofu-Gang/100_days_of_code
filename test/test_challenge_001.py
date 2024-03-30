5024ecf5c5701dd63d22153f9232caa8e4f96f74
Jakub Franěk <tofugangsw@gmail.com>
2024-03-30 12:12:39 +0100

    Refactoring

--- test/test_challenge_001.py
+++ test/test_challenge_001.py
@@ -1,10 +1,11 @@
 import unittest
+
 from src.day_001_band_name_generator.band_name_generator import generate_band_name
 
 
 ########################################################################################################################
 
-class TestChallenge1(unittest.TestCase):
+class TestChallenge001(unittest.TestCase):
     def test_generate_band_name(self):
         city_name = "Pilsen"
         pet_name = "Miksi"
