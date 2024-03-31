c1d87c37a6f85bbe2d8fb83e2182b42a4a42c22c
Jakub Franěk <tofugangsw@gmail.com>
2024-03-31 14:33:21 +0200

    Error handling for collecting the total bill, tip and number of people values.

--- src/day_002_tip_calculator/tip_calculator.py
+++ src/day_002_tip_calculator/tip_calculator.py
@@ -19,9 +19,37 @@ def run_program() -> None:
     """
 
     print("Welcome to the tip calculator!")
-    total_bill = float(input("What was the total bill? $"))
-    tip = int(input("How much tip would you like to give? 10, 12, or 15? >"))
-    people_count = int(input("How many people to split the bill? >"))
+
+    total_bill = None
+    while True:
+        try:
+            total_bill = float(input("What was the total bill? $"))
+            break
+        except ValueError:
+            print("Invalid value. Integer or a real number with decimal point required.")
+            continue
+
+    tip = None
+    while True:
+        try:
+            tip = int(input("How much tip would you like to give? 10, 12, or 15? > "))
+            if tip not in (10, 12, 15):
+                raise ValueError
+            else:
+                break
+        except ValueError:
+            print("Invalid value. Integer 10, 12 or 15 required.")
+            continue
+
+    people_count = None
+    while True:
+        try:
+            people_count = int(input("How many people to split the bill? > "))
+            break
+        except ValueError:
+            print("Invalid value. Integer required.")
+            continue
+
     print("Each person should pay: ${:.2f}".format(calculate_tip(total_bill, tip, people_count)))
 
 
