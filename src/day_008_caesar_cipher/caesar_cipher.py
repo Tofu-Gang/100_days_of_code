70ebff78960d755f48f12096df23404eacbb5adb
Jakub Franěk <tofugangsw@gmail.com>
2024-04-03 00:04:50 +0200

    Refactoring; user input checks.

--- src/day_008_caesar_cipher/caesar_cipher.py
+++ src/day_008_caesar_cipher/caesar_cipher.py
@@ -27,10 +27,9 @@ def _encode_dialogue() -> None:
     Get a message and the shift number from the user. Then, encode the message with the Caesar cipher.
     """
 
-    print("Type your message:")
+    print("Type your message (only letters are encoded, the rest stays the same; will be converted to lower case):")
     message = input("> ")
-    print("Type the shift number:")
-    offset = int(input("> "))
+    offset = _get_shift_number()
     print("Here's the encoded result: " + _encode_message(message, offset))
 
 
@@ -41,11 +40,27 @@ def _decode_dialogue() -> None:
     Get a message encoded with the Caesar cipher and the shift number from the user. Then, decode it.
     """
 
-    print("Type your message:")
+    print("Type your message (only letters are decoded, the rest stays the same; will be converted to lower case):")
     message = input("> ")
-    print("Type the shift number:")
-    offset = int(input("> "))
+    offset = _get_shift_number()
     print("Here's the decoded result: " + _decode_message(message, offset))
+
+
+########################################################################################################################
+
+def _get_shift_number() -> int:
+    """
+    :return: shift number for the Caesar cipher from the user
+    """
+
+    while True:
+        print("Type the shift number:")
+        try:
+            offset = int(input("> "))
+            return offset
+        except ValueError:
+            print("Invalid value; integer required.")
+            continue
 
 
 ########################################################################################################################
@@ -59,7 +74,7 @@ def _encode_message(message: str, offset: int) -> str:
 
     result = ""
 
-    for character in message:
+    for character in message.strip().lower():
         if character in ALPHABET:
             index = (ALPHABET.index(character) + offset) % len(ALPHABET)
             result += ALPHABET[index]
@@ -80,7 +95,7 @@ def _decode_message(message: str, offset: int) -> str:
 
     result = ""
 
-    for character in message:
+    for character in message.strip().lower():
         if character in ALPHABET:
             index = ALPHABET.index(character) - offset
             if index < 0:
@@ -107,6 +122,9 @@ def run_program() -> None:
             _encode_dialogue()
         elif choice in ("d", "decode"):
             _decode_dialogue()
+        else:
+            print("Invalid value. Try again.")
+            continue
 
         print("Press Enter to continue or Ctrl+C to exit.")
         input()
