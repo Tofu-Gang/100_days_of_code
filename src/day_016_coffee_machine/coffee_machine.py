4dfd5fdaca2555886bb281780e95d345b1b7b06e
Jakub Franěk <tofugangsw@gmail.com>
2024-04-09 21:35:04 +0200

    Angela's code added; her main.py renamed to coffee_machine.py so it works in my framework.

---
+++ src/day_016_coffee_machine/coffee_machine.py
@@ -0,0 +1,16 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+
+########################################################################################################################
+
+def run_program() -> None:
+    """
+    Make coffee machine from the code by Angela Yu.
+    """
+
+    pass
+
+
+########################################################################################################################
