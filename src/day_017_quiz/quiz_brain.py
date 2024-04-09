baafefe7fd1b15aeccea2818f00b06f7bcac9d6b
Jakub Franěk <tofugangsw@gmail.com>
2024-04-09 23:23:08 +0200

    Method that displays a question and gets an answer from the user implemented.

--- src/day_017_quiz/quiz_brain.py
+++ src/day_017_quiz/quiz_brain.py
@@ -1,3 +1,5 @@
+from random import choice
+
 from .data import question_data, TEXT_KEY, ANSWER_KEY
 from .question_model import Question
 
@@ -11,8 +13,39 @@ class QuizzBrain:
         Set the questions counter for display purposes. Load question bank.
         """
 
-        self._question_number = 0
+        self._question_number = 1
         self._question_bank = list(Question(question[TEXT_KEY], bool(question[ANSWER_KEY]))
                                    for question in question_data)
+
+########################################################################################################################
+
+    def next_question(self) -> bool | None:
+        """
+        Display the question. Get the answer from the user.
+
+        :return: True for correct answer, False for incorrect, None if no question can be displayed.
+        """
+
+        try:
+            question = choice(self._question_bank)
+
+            while True:
+                answer = input(f"Q.{self._question_number}: {question.text} (True/False)?: ").strip().upper()
+
+                if answer in ("T", "TRUE"):
+                    answer = True
+                    break
+                elif answer in ("F", "FALSE"):
+                    answer = False
+                    break
+                else:
+                    print("Invalid value. Please try again.")
+
+            self._question_bank.remove(question)
+            self._question_number += 1
+            return bool(answer) == question.answer
+        except IndexError:
+            print("No more questions. You win the game!")
+            return None
 
 ########################################################################################################################
