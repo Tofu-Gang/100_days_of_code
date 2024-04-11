0e9c8904d8fa1fc40b9f382d0cbc08756ce16da1
Jakub Franěk <tofugangsw@gmail.com>
2024-04-12 00:20:32 +0200

    Draw a triangle, square, pentagon, hexagon, heptagon, octagon, nonagon and decagon with Turtle. Each shape uses a different, random line color.

--- src/day_018_damien_hirst/damien_hirst.py
+++ src/day_018_damien_hirst/damien_hirst.py
@@ -1,4 +1,5 @@
-from turtle import Turtle, Screen
+from turtle import Turtle, Screen, colormode
+from random import randint
 
 
 ########################################################################################################################
@@ -20,6 +21,28 @@ def dashed_line(turtle: Turtle) -> None:
 
 ########################################################################################################################
 
+def draw_shape(turtle: Turtle, side_size: int, sides_count: int) -> None:
+    """
+    Draw a shape with specified number and size of sides. Each shape is drawn with a different, random color.
+
+    :param turtle: a Turtle object
+    :param side_size: size of a shape side
+    :param sides_count: number of shape sides
+    """
+
+    colormode(255)
+    r = randint(0, 255)
+    g = randint(0, 255)
+    b = randint(0, 255)
+    turtle.pencolor((r, g, b))
+    angle = 360 / sides_count
+    for _ in range(sides_count):
+        turtle.forward(side_size)
+        turtle.right(angle)
+
+
+########################################################################################################################
+
 
 def run_program() -> None:
     """
@@ -27,6 +50,14 @@ def run_program() -> None:
     """
 
     timmy = Turtle()
+    draw_shape(timmy, 100, 3)
+    draw_shape(timmy, 100, 4)
+    draw_shape(timmy, 100, 5)
+    draw_shape(timmy, 100, 6)
+    draw_shape(timmy, 100, 7)
+    draw_shape(timmy, 100, 8)
+    draw_shape(timmy, 100, 9)
+    draw_shape(timmy, 100, 10)
     dashed_line(timmy)
     screen = Screen()
     screen.exitonclick()
