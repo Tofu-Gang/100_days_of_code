131e3de8bf0d724efffaec72625d6ee86817990e
Jakub Franěk <tofugangsw@gmail.com>
2024-04-12 00:16:33 +0200

    Draw a dashed line with Turtle.

--- src/day_018_damien_hirst/damien_hirst.py
+++ src/day_018_damien_hirst/damien_hirst.py
@@ -3,6 +3,23 @@ from turtle import Turtle, Screen
 
 ########################################################################################################################
 
+def dashed_line(turtle: Turtle) -> None:
+    """
+    Draw a dashed line.
+
+    :param turtle: a Turtle object
+    """
+
+    turtle.left(90)
+    for _ in range(30):
+        turtle.forward(5)
+        turtle.penup()
+        turtle.forward(5)
+        turtle.pendown()
+
+
+########################################################################################################################
+
 
 def run_program() -> None:
     """
@@ -10,6 +27,7 @@ def run_program() -> None:
     """
 
     timmy = Turtle()
+    dashed_line(timmy)
     screen = Screen()
     screen.exitonclick()
 
