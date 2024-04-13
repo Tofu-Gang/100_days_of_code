acecb1ecf0171e2024f4ccb78402925231c71b2e
Jakub Franěk <tofugangsw@gmail.com>
2024-04-13 19:55:10 +0200

    Paint an image similar to Damien Hirst's "spots"; 10x10 dots in random colors.

--- src/day_018_damien_hirst/damien_hirst.py
+++ src/day_018_damien_hirst/damien_hirst.py
@@ -1,22 +1,5 @@
 from turtle import Turtle, Screen, colormode
-from random import randint, choice
-
-
-########################################################################################################################
-
-def dashed_line(turtle: Turtle) -> None:
-    """
-    Draw a dashed line.
-
-    :param turtle: a Turtle object
-    """
-
-    turtle.left(90)
-    for _ in range(30):
-        turtle.forward(5)
-        turtle.penup()
-        turtle.forward(5)
-        turtle.pendown()
+from random import randint
 
 
 ########################################################################################################################
@@ -26,7 +9,6 @@ def random_color() -> tuple[int, int, int]:
     :return:
     """
 
-    colormode(255)
     r = randint(0, 255)
     g = randint(0, 255)
     b = randint(0, 255)
@@ -35,71 +17,29 @@ def random_color() -> tuple[int, int, int]:
 
 ########################################################################################################################
 
-def draw_shape(turtle: Turtle, side_size: int, sides_count: int) -> None:
-    """
-    Draw a shape with specified number and size of sides. Each shape is drawn with a different, random color.
-
-    :param turtle: a Turtle object
-    :param side_size: size of a shape side
-    :param sides_count: number of shape sides
-    """
-
-    turtle.pencolor(random_color())
-    angle = 360 / sides_count
-    for _ in range(sides_count):
-        turtle.forward(side_size)
-        turtle.right(angle)
-
-
-########################################################################################################################
-
-def random_walk(turtle: Turtle) -> None:
-    """
-
-    :param turtle:
-    """
-
-    turtle.pensize(10)
-
-    for _ in range(200):
-        turtle.pencolor(random_color())
-        turtle.right(choice((90, 180, 270, 360)))
-        turtle.forward(20)
-
-
-########################################################################################################################
-
-def draw_spirograph(turtle: Turtle) -> None:
-    """
-
-    :param turtle:
-    """
-
-    step = 5
-    steps = int(360 / step)
-
-    for _ in range(steps):
-        color = random_color()
-        turtle.pencolor(color)
-        turtle.fillcolor(color)
-        turtle.begin_fill()
-        turtle.circle(100)
-        turtle.end_fill()
-        turtle.right(step)
-
-
-########################################################################################################################
-
-
 def run_program() -> None:
     """
-
+    Paint an image similar to Damien Hirst's "spots"; 10x10 dots in random colors.
     """
 
     timmy = Turtle()
     timmy.speed("fastest")
-    draw_spirograph(timmy)
+    timmy.penup()
+    timmy.hideturtle()
+
+    colormode(255)
     screen = Screen()
+    screen.setup(470, 450)
+    screen.bgcolor((244, 244, 244))
+
+    for x in range(-200, 200, 40):
+        for y in range(-200, 200, 40):
+            timmy.setx(x)
+            timmy.sety(y)
+            color = random_color()
+            timmy.dot(20, color)
+            timmy.forward(40)
+
     screen.exitonclick()
 
 
