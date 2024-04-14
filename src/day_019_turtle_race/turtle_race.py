3f45a2ea31a8c3aa3930aee9e7722f0587d81503
Jakub Franěk <tofugangsw@gmail.com>
2024-04-14 19:41:22 +0200

    Make six turtles, each a different color; move them to the left edge of the screen and space them a bit vertically.

--- src/day_019_turtle_race/turtle_race.py
+++ src/day_019_turtle_race/turtle_race.py
@@ -3,6 +3,8 @@ from turtle import Turtle, Screen
 SCREEN_WIDTH = 500
 SCREEN_HEIGHT = 400
 TURTLE_WIDTH = 10
+TURTLES_DISTANCE = 40
+COLORS = ("red", "orange", "yellow", "green", "blue", "purple")
 
 ########################################################################################################################
 
@@ -12,13 +14,14 @@ def run_program() -> None:
 
     """
 
-    timmy = Turtle(shape="turtle")
+    turtles = tuple(Turtle(shape="turtle") for _ in range(len(COLORS)))
+    [turtles[i].color(COLORS[i]) for i in range(len(turtles))]
+    [turtle.penup() for turtle in turtles]
+    [turtles[i].goto(-(SCREEN_WIDTH / 2) + TURTLE_WIDTH, (2 * TURTLES_DISTANCE + TURTLES_DISTANCE / 2) - i * TURTLES_DISTANCE) for i in range(len(turtles))]
     screen = Screen()
     screen.setup(width=SCREEN_WIDTH, height=SCREEN_HEIGHT)
     user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color:")
     print(user_bet)
-    timmy.penup()
-    timmy.goto(-(SCREEN_WIDTH / 2) + TURTLE_WIDTH, 0)
     screen.exitonclick()
 
 
