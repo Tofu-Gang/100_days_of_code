c4528c2635a91e3a254a6a901fd44b37aad02481
Jakub Franěk <tofugangsw@gmail.com>
2024-04-14 19:01:24 +0200

    Etch & Sketch class moved to another file.

--- src/day_019_turtle_race/turtle_race.py
+++ src/day_019_turtle_race/turtle_race.py
@@ -1,40 +1,5 @@
-from turtle import Turtle, Screen
+from .etch_and_sketch import EtchAndSketch
 
-
-########################################################################################################################
-
-class EtchAndSketch:
-
-    def __init__(self):
-        """
-
-        """
-
-        self._timmy = Turtle()
-        self._screen = Screen()
-        self._screen.onkeypress(lambda: self._timmy.forward(10), "w")
-        self._screen.onkeypress(lambda: self._timmy.backward(10), "s")
-        self._screen.onkeypress(lambda: self._timmy.left(5), "a")
-        self._screen.onkeypress(lambda: self._timmy.right(5), "d")
-        self._screen.onkeypress(self._clear, "c")
-        self._screen.listen()
-        self._screen.exitonclick()
-
-########################################################################################################################
-
-    def _clear(self):
-        """
-
-        :return:
-        """
-
-        self._timmy.penup()
-        self._timmy.clear()
-        self._timmy.home()
-        self._timmy.pendown()
-
-
-########################################################################################################################
 
 def run_program() -> None:
     """
