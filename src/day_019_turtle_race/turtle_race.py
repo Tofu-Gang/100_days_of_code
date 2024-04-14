10b4676457a4b1c92f7bf67d0d64a0d9840b9dd6
Jakub Franěk <tofugangsw@gmail.com>
2024-04-14 19:17:05 +0200

    Turtle race class created. Prompt the user on which color turtle they bet. Print the bet for now.

--- src/day_019_turtle_race/turtle_race.py
+++ src/day_019_turtle_race/turtle_race.py
@@ -1,4 +1,27 @@
-from .etch_and_sketch import EtchAndSketch
+from turtle import Turtle, Screen
+
+
+########################################################################################################################
+
+class TurtleRace:
+    SCREEN_WIDTH = 500
+    SCREEN_HEIGHT = 400
+
+########################################################################################################################
+
+    def __init__(self):
+        """
+
+        """
+
+        self._timmy = Turtle()
+        self._screen = Screen()
+        self._screen.setup(width=self.SCREEN_WIDTH, height=self.SCREEN_HEIGHT)
+        self._user_bet = self._screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color:")
+        print(self._user_bet)
+        self._screen.exitonclick()
+
+########################################################################################################################
 
 
 def run_program() -> None:
@@ -6,7 +29,7 @@ def run_program() -> None:
 
     """
 
-    EtchAndSketch()
+    TurtleRace()
 
 
 ########################################################################################################################
