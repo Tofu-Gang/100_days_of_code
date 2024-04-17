02432e65416e5d6e78dc00aa716212ba662f3f2d
Jakub Franěk <tofugangsw@gmail.com>
2024-04-17 11:46:43 +0200

    Refactoring.

---
+++ src/day_022_pong/ball.py
@@ -0,0 +1,44 @@
+from turtle import Turtle
+from random import randint
+from time import sleep
+
+
+########################################################################################################################
+
+class Ball(Turtle):
+    WIDTH = 20
+    SPEED = 50
+    STEP = 5
+
+########################################################################################################################
+
+    def __init__(self, screen):
+        """
+        Create the pong ball.
+        """
+
+        super().__init__(shape="circle")
+        self.color("white")
+        self.penup()
+        self._is_moving = False
+
+        self._screen = screen
+
+########################################################################################################################
+
+    def start_moving(self) -> None:
+        """
+        Choose a random direction and move the ball in that direction.
+        """
+
+        self._is_moving = True
+        angle = randint(0, 359)
+        self.left(angle)
+
+        while self._is_moving:
+            self.forward(self.STEP)
+            self._screen.update()
+            sleep(1 / self.SPEED)
+
+
+########################################################################################################################
