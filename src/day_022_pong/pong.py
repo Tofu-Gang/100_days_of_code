22a7d9ec5c7190768848e2c31eceb5320cee0698
Jakub Franěk <tofugangsw@gmail.com>
2024-04-17 11:31:25 +0200

    Create both paddles.

--- src/day_022_pong/pong.py
+++ src/day_022_pong/pong.py
@@ -1,4 +1,20 @@
-from turtle import Screen
+from turtle import Screen, Turtle
+
+
+########################################################################################################################
+
+class Paddle(Turtle):
+    WIDTH = 20
+
+    def __init__(self):
+        """
+        Create a pong paddle.
+        """
+
+        super().__init__(shape="square")
+        self.color("white")
+        self.penup()
+        self.shapesize(4, 1)
 
 
 ########################################################################################################################
@@ -19,6 +35,14 @@ class Pong:
         self._screen.bgcolor("black")
         self._screen.title("PONG")
         self._screen.tracer(0)
+
+        self._paddle_left = Paddle()
+        self._paddle_left.goto(-self.SCREEN_WIDTH / 2 + Paddle.WIDTH, 0)
+        self._paddle_right = Paddle()
+        # try to place the right paddle in the same distance from its edge as is the left one, the screen geometry works
+        # weird, that's why there is the 1.4 coefficient
+        self._paddle_right.goto(self.SCREEN_WIDTH / 2 - Paddle.WIDTH * 1.4, 0)
+
         self._screen.update()
 
 ########################################################################################################################
