114fc58e19ec72bdcf103e90e80e5f0aa24af340
Jakub Franěk <tofugangsw@gmail.com>
2024-04-19 16:14:05 +0200

    Game controls implemented. Turtle moves up when up arrow pressed, game ends with escape press.

--- src/day_023_turtle_crossing/player.py
+++ src/day_023_turtle_crossing/player.py
@@ -1,4 +1,4 @@
-from turtle import Turtle
+from turtle import Turtle, Screen
 
 
 ########################################################################################################################
@@ -6,6 +6,7 @@ from turtle import Turtle
 class Player(Turtle):
     NORTH = 90
     WIDTH = 20
+    STEP = 5
 
 ########################################################################################################################
 
@@ -17,5 +18,39 @@ class Player(Turtle):
         super().__init__(shape="turtle")
         self.setheading(self.NORTH)
         self.penup()
+
+        self._screen = Screen()
+        self._screen.onkeypress(self._start_movement, "Up")
+        self._screen.onkeyrelease(self._stop_movement, "Up")
+
+        self._movement_delta = 0
+
+########################################################################################################################
+
+    def _start_movement(self) -> None:
+        """
+
+        """
+
+        self._movement_delta = 1
+
+########################################################################################################################
+
+    def _stop_movement(self) -> None:
+        """
+
+        """
+
+        self._movement_delta = 0
+
+########################################################################################################################
+
+    def move(self) -> None:
+        """
+
+        """
+
+        self.forward(self.STEP * self._movement_delta)
+        self._screen.update()
 
 ########################################################################################################################
--- src/day_023_turtle_crossing/turtle_crossing.py
+++ src/day_023_turtle_crossing/turtle_crossing.py
@@ -1,4 +1,5 @@
 from turtle import Screen
+from time import sleep
 
 from .player import Player
 
@@ -8,6 +9,7 @@ from .player import Player
 class TurtleCrossing:
     SCREEN_WIDTH = 600
     SCREEN_HEIGHT = 600
+    SPEED = 50
 
 ########################################################################################################################
 
@@ -19,20 +21,36 @@ class TurtleCrossing:
         self._screen = Screen()
         self._screen.setup(width=self.SCREEN_WIDTH, height=self.SCREEN_HEIGHT)
         self._screen.title("TURTLE CROSSING")
+        self._screen.onkeypress(self._end_game, "Escape")
         self._screen.tracer(0)
+        self._screen.listen()
 
         self._player = Player()
         self._player.goto(0, -self.SCREEN_HEIGHT / 2 + Player.WIDTH)
         self._screen.update()
 
+        self._game_going = True
+
 ########################################################################################################################
 
-    def end_game(self) -> None:
+    def start_game(self) -> None:
+        """
+        Start the game.
+        """
+
+        while self._game_going:
+            self._player.move()
+            sleep(1 / self.SPEED)
+
+########################################################################################################################
+
+    def _end_game(self) -> None:
         """
         End the game.
         """
 
-        self._screen.exitonclick()
+        self._game_going = False
+        self._screen.bye()
 
 
 ########################################################################################################################
@@ -43,7 +61,7 @@ def run_program() -> None:
     """
 
     game = TurtleCrossing()
-    game.end_game()
+    game.start_game()
 
 
 ########################################################################################################################
