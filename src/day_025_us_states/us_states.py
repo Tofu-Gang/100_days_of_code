b9c5b36de6b3d95d203df05fa23361070f6c8982
Jakub Franěk <tofugangsw@gmail.com>
2024-04-24 17:35:29 +0200

    Comments, refactoring.

--- src/day_025_us_states/us_states.py
+++ src/day_025_us_states/us_states.py
@@ -17,7 +17,10 @@ class US_States:
 ########################################################################################################################
     def __init__(self):
         """
-
+        Set up the window with an image of a map of the USA. Create turtles for writing text in the window. Establish
+        connection between mouse key press and escape key and their respective functions (guess a state name dialog and
+        closing the window). Keep track of the player score (how many states are guessed correctly so far). Load the
+        states data from a csv file (state labels and their position in the map).
         """
 
         self._screen = Screen()
@@ -34,8 +37,8 @@ class US_States:
         self._state_name_turtle.penup()
         self._state_name_turtle.hideturtle()
 
-        self._text_turtle = Turtle()
-        self._clear_text()
+        self._info_turtle = Turtle()
+        self._clear_info()
 
         self._states_data = read_csv(self.STATES_FILE_PATH)
         self._score = 0
@@ -43,14 +46,14 @@ class US_States:
 
 ########################################################################################################################
 
-    def _clear_text(self) -> None:
+    def _clear_info(self) -> None:
+        """
+        Clear all info text (anything other than state labels) and prepare the text turtle for writing another message.
         """
 
-        """
-
-        self._text_turtle.reset()
-        self._text_turtle.penup()
-        self._text_turtle.hideturtle()
+        self._info_turtle.reset()
+        self._info_turtle.penup()
+        self._info_turtle.hideturtle()
 
 ########################################################################################################################
 
@@ -62,24 +65,33 @@ class US_States:
 
         if self._score < len(self._states_data):
             state_name = self._screen.textinput(title=f"{self._score}/{len(self._states_data)} States Correct",
-                                                prompt="What's another state's name?").title()
-            row = self._states_data[self._states_data["state"] == state_name]
+                                                prompt="What's another state's name?")
+            # do nothing if the user closed the popup window (in which case the state_name IS None)
+            if state_name is not None:
+                state_name = state_name.title()
+                row = self._states_data[self._states_data["state"] == state_name]
 
-            if len(row) == 1:
-                self._state_name_turtle.goto(row.x.item(), row.y.item())
-                self._state_name_turtle.write(state_name, font=("Courier", 12, "bold"))
-                self._score += 1
+                if len(row) == 1:
+                    # correct guess
+                    self._state_name_turtle.goto(row.x.item(), row.y.item())
+                    self._state_name_turtle.write(state_name, font=("Courier", 12, "bold"))
+                    self._score += 1
 
-                if self._score == len(self._states_data):
-                    self._text_turtle.color("red")
-                    self._text_turtle.goto(0, 0)
-                    self._text_turtle.write("YOU WIN!", align="center", font=("Courier", 40, "bold"))
-            else:
-                self._text_turtle.color("red")
-                self._text_turtle.goto(0, 0)
-                self._text_turtle.write("WRONG!", align="center", font=("Courier", 40, "bold"))
-                sleep(2)
-                self._clear_text()
+                    if self._score == len(self._states_data):
+                        # all states guessed correctly
+                        self._info_turtle.color("red")
+                        self._info_turtle.goto(0, 0)
+                        self._info_turtle.write("YOU WIN!", align="center", font=("Courier", 40, "bold"))
+                else:
+                    # wrong guess
+                    self._info_turtle.color("red")
+                    self._info_turtle.goto(0, 0)
+                    self._info_turtle.write("WRONG!", align="center", font=("Courier", 40, "bold"))
+                    sleep(2)
+                    self._clear_info()
+        # for some reason pressing the escape key does no longer work after at least one mouse key press in the window
+        # and this function call, this ensures the escape key connection with turtle.bye() again
+        self._screen.listen()
 
 
 ########################################################################################################################
