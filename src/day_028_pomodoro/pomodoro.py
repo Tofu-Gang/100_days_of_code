b8d3af6bf0f605d6ee2bf6c86676b803fefb872f
Jakub Franěk <tofugangsw@gmail.com>
2024-04-29 02:15:49 +0200

    Refactoring.

--- src/day_028_pomodoro/pomodoro.py
+++ src/day_028_pomodoro/pomodoro.py
@@ -22,24 +22,11 @@ class Pomodoro:
 
     def __init__(self):
         """
-        Create the instance variables and call the respective function to set them up. First do this for the passive,
-        display widgets (background image, labels, etc...) and then for controls (buttons).
-        Finally, show the main window.
+        Set up the display widgets and window controls. Finally, show the main window.
         """
 
-        self._window = Tk()
-        self._canvas = Canvas()
-        # could be set as a local variable, but I guess the garbage collector deletes it and then the image won't get
-        # displayed at all
-        self._tomato_img = PhotoImage(file=self.BG_IMAGE_FILE_PATH)
-        self._info_label = Label()
-        self._progress_label = Label()
         self._set_display()
-
-        self._start_button = Button()
-        self._reset_button = Button()
         self._set_controls()
-
         self._window.mainloop()
 
 ########################################################################################################################
@@ -50,9 +37,13 @@ class Pomodoro:
         the tomato picture and progress label (checkmarks) between the buttons.
         """
 
+        self._window = Tk()
         self._window.title("Pomodoro")
         self._window.configure(padx=self.WINDOW_PAD_X, pady=self.WINDOW_PAD_Y, bg=self.YELLOW)
 
+        # could be set as a local variable, but I guess the garbage collector deletes it and then the image won't get
+        # displayed at all
+        self._tomato_img = PhotoImage(file=self.BG_IMAGE_FILE_PATH)
         tomato_width = self._tomato_img.width()
         tomato_height = self._tomato_img.height()
         tomato_pos_x = tomato_width / 2
@@ -61,15 +52,15 @@ class Pomodoro:
         counter_label_pos_x = tomato_pos_x
         counter_label_pos_y = tomato_pos_y + 18
 
-        self._canvas.configure(width=tomato_width, height=tomato_height, bg=self.YELLOW, highlightthickness=0)
+        self._canvas = Canvas(width=tomato_width, height=tomato_height, bg=self.YELLOW, highlightthickness=0)
         self._canvas.create_image(tomato_pos_x, tomato_pos_y, image=self._tomato_img)
         self._canvas.create_text(counter_label_pos_x, counter_label_pos_y,
                                  text="00:00", fill="white", font=self.COUNTER_FONT)
         self._canvas.grid(row=1, column=1)
 
-        self._info_label.configure(text="Timer", fg=self.GREEN, bg=self.YELLOW, font=self.INFO_FONT)
+        self._info_label = Label(text="Timer", fg=self.GREEN, bg=self.YELLOW, font=self.INFO_FONT)
         self._info_label.grid(row=0, column=1)
-        self._progress_label.configure(text="\u2714", fg=self.GREEN, bg=self.YELLOW)
+        self._progress_label = Label(text="\u2714", fg=self.GREEN, bg=self.YELLOW)
         self._progress_label.grid(row=3, column=1)
 
 ########################################################################################################################
@@ -79,9 +70,9 @@ class Pomodoro:
         Create the start and reset buttons, connect them to their respective methods and place them in the main window.
         """
 
-        self._start_button.configure(text="Start", command=self._start, highlightthickness=0)
+        self._start_button = Button(text="Start", command=self._start, highlightthickness=0)
         self._start_button.grid(row=2, column=0)
-        self._reset_button.configure(text="Reset", command=self._reset, highlightthickness=0)
+        self._reset_button = Button(text="Reset", command=self._reset, highlightthickness=0)
         self._reset_button.grid(row=2, column=2)
 
 ########################################################################################################################
