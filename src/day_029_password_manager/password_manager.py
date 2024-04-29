b8c8a2aec501050a992b883299a44d7592b045bb
Jakub Franěk <tofugangsw@gmail.com>
2024-04-29 13:46:58 +0200

    Set up grid layout.

--- src/day_029_password_manager/password_manager.py
+++ src/day_029_password_manager/password_manager.py
@@ -1,4 +1,4 @@
-from tkinter import Tk, PhotoImage, Canvas
+from tkinter import Tk, PhotoImage, Canvas, Label, Entry, Button
 from os.path import join, dirname, realpath
 
 
@@ -6,7 +6,7 @@ from os.path import join, dirname, realpath
 
 class PasswordManager:
     _BG_IMAGE_FILE_PATH = join(dirname(realpath(__file__)), "logo.png")
-    _WINDOW_PADDING = 20
+    _WINDOW_PADDING = 50
     _LOGO_WIDTH = 200
     _LOGO_HEIGHT = 200
 
@@ -36,7 +36,17 @@ class PasswordManager:
         self._logo_img = PhotoImage(file=self._BG_IMAGE_FILE_PATH)
         self._canvas = Canvas(width=self._LOGO_WIDTH, height=self._LOGO_HEIGHT, highlightthickness=0)
         self._canvas.create_image(self._LOGO_WIDTH / 2, self._LOGO_HEIGHT / 2, image=self._logo_img)
-        self._canvas.pack()
+        self._canvas.grid(row=0, column=1)
+
+        Label(text="Website:").grid(row=1, column=0)
+        Label(text="Email/Username:").grid(row=2, column=0)
+        Label(text="Password:").grid(row=3, column=0)
+
+        Entry().grid(row=1, column=1, columnspan=2, sticky="EW")
+        Entry().grid(row=2, column=1, columnspan=2, sticky="EW")
+        Entry().grid(row=3, column=1, sticky="EW")
+        Button(text="Generate Password").grid(row=3, column=2)
+        Button(text="Add").grid(row=4, column=1, columnspan=2, sticky="EW")
 
 
 ########################################################################################################################
