c588ef076bde7493550b58924ddbf1fcfa0c6a41
Jakub Franěk <tofugangsw@gmail.com>
2024-05-08 13:58:24 +0200

    Display question text and current score.

--- src/day_034_quiz_gui/quiz_gui.py
+++ src/day_034_quiz_gui/quiz_gui.py
@@ -21,6 +21,7 @@ class QuizGUI:
         self._set_display()
         self._set_controls()
         self._quiz_brain = QuizzBrain()
+        self._next_question()
         self._window.mainloop()
 
 ########################################################################################################################
@@ -34,12 +35,12 @@ class QuizGUI:
         self._window.title("Quizzler")
         self._window.config(padx=20, pady=20, bg=self._THEME_COLOR)
 
-        self._score_label = Label(text="Score: 0", bg=self._THEME_COLOR, fg="white")
+        self._score_label = Label(bg=self._THEME_COLOR, fg="white")
         self._score_label.grid(row=0, column=1)
 
         self._canvas = Canvas(width=300, height=250, bg="white")
         self._question_text = self._canvas.create_text(
-            150, 125, text="Some question text", fill=self._THEME_COLOR, font=("Arial", 20, "italic"))
+            150, 125, width=280, fill=self._THEME_COLOR, font=("Arial", 20, "italic"))
         self._canvas.grid(row=1, column=0, columnspan=2, pady=50)
 
 ########################################################################################################################
@@ -51,19 +52,37 @@ class QuizGUI:
 
         self._true_image = PhotoImage(file=self._TRUE_IMAGE_FILE_PATH)
         self._false_image = PhotoImage(file=self._FALSE_IMAGE_FILE_PATH)
-        Button(image=self._true_image, highlightthickness=0, command=lambda: self._check_answer(True)).grid(row=2, column=0)
-        Button(image=self._false_image, highlightthickness=0, command=lambda: self._check_answer(False)).grid(row=2, column=1)
+        Button(image=self._true_image,
+               highlightthickness=0,
+               command=lambda: self._check_answer(True)).grid(row=2, column=0)
+        Button(image=self._false_image,
+               highlightthickness=0,
+               command=lambda: self._check_answer(False)).grid(row=2, column=1)
 
 ########################################################################################################################
 
     def _check_answer(self, answer: bool) -> None:
         """
+        Pass the user answer to the quizz brain to evaluate it. Move on to the next question.
 
-        :param answer:
-        :return:
+        :param answer: user answer; True for correct, False for incorrect
         """
 
-        pass
+        self._quiz_brain.check_answer(answer)
+        self._next_question()
+
+########################################################################################################################
+
+    def _next_question(self) -> None:
+        """
+        Move on to the next question. Update the current score.
+        """
+
+        self._quiz_brain.next_question()
+        self._score_label.configure(text=f"Score: {self._quiz_brain.score}", fg="white", bg=self._THEME_COLOR)
+        self._canvas.itemconfig(
+            self._question_text,
+            text=f"Q.{self._quiz_brain.questions_passed}: {self._quiz_brain.current_question.text}")
 
 
 ########################################################################################################################
