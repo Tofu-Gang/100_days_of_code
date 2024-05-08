fcf7d3b7d7e40f319dc586d2f7884f4bad18e584
Jakub Franěk <tofugangsw@gmail.com>
2024-05-08 11:51:16 +0200

    Refactor day 17 challenge to MVC pattern. Therefore we can reuse the question model class and the "brain" of the quiz class and just create a new GUI.

--- src/day_017_quiz/quiz_brain.py
+++ src/day_017_quiz/quiz_brain.py
@@ -12,17 +12,16 @@ class QuizzBrain:
 
     def __init__(self):
         """
-        Set the questions counter for display purposes. Set the user score to zero. Load question bank. Create temporary
-        variables where the current question and user answer will be stored between function calls.
+        Set the questions and score counters to zero. Load question bank. Create temporary variable where the current
+        question will be stored between function calls.
         """
 
+        self._question = None
         self._question_number = 0
+        self._score = 0
         opentdb_questions = loads(get("https://opentdb.com/api.php?amount=20&type=boolean").content)["results"]
         self._question_bank = list(Question(unescape(question["question"]), eval(question["correct_answer"]))
                                    for question in opentdb_questions)
-        self._score = 0
-        self._question = None
-        self._answer = None
 
 ########################################################################################################################
 
@@ -46,6 +45,17 @@ class QuizzBrain:
 
 ########################################################################################################################
 
+    @property
+    def current_question(self) -> Question:
+        """
+        :return: current question object
+        """
+
+        return self._question
+
+########################################################################################################################
+
+    @property
     def still_has_questions(self) -> bool:
         """
         :return: True if there are questions left, False otherwise
@@ -55,45 +65,27 @@ class QuizzBrain:
 
 ########################################################################################################################
 
-    def check_answer(self) -> None:
+    def check_answer(self, answer: bool) -> bool:
         """
-        Checks the user answer and displays the result.
+        :param answer: user answer
+        :return: True if the user answer was correct, False otherwise
         """
 
-        if self._question.answer == self._answer:
-            print("You got it right!")
+        if self._question.answer == answer:
             self._score += 1
+            return True
         else:
-            print("That's wrong.")
-        print(f"The correct answer was: {self._question.answer}.")
-        if self.still_has_questions():
-            print(f"Your current score is: {self._score}/{self._question_number}")
-
-        self._question = None
-        self._answer = None
+            return False
 
 ########################################################################################################################
 
     def next_question(self) -> None:
         """
-        Display the question. Get the answer from the user.
+        Move on to the next question.
         """
 
         self._question_number += 1
         self._question = choice(self._question_bank)
-
-        while True:
-            answer = input(f"Q.{self._question_number}: {self._question.text} (True/False)?: ").strip().upper()
-
-            if answer in ("T", "TRUE"):
-                self._answer = True
-                break
-            elif answer in ("F", "FALSE"):
-                self._answer = False
-                break
-            else:
-                print("Invalid value. Please try again.")
-
         self._question_bank.remove(self._question)
 
 ########################################################################################################################
